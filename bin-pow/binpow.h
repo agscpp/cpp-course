d6828c8fdf8ac64259cf67166b5028bf0d801597
agaevgs_y <agaevgs@yandex-team.ru>
2023-09-24 16:12:37 +0000

    Replace tabs with spaces

--- bin-pow/binpow.h
+++ bin-pow/binpow.h
@@ -3,17 +3,17 @@
 #include <cmath>
 
 int BinPow(int a, int64_t b, int c) {
-	int64_t res = 1;
+    int64_t res = 1;
     int64_t cur = static_cast<int64_t>(a);
 
-	while (b) {
-		if (b & 1) {
-			res = (res * cur) % c;
+    while (b) {
+        if (b & 1) {
+            res = (res * cur) % c;
             res %= c;
         }
-		cur = (cur * cur) % c;
-		b >>= 1;
-	}
+        cur = (cur * cur) % c;
+        b >>= 1;
+    }
 
-	return static_cast<int>(res);
+    return static_cast<int>(res);
 }
