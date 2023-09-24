e941a1e617f7e3e36a2cc62a41a6d4a5c5b5a322
agaevgs_y <agaevgs@yandex-team.ru>
2023-09-24 20:49:46 +0000

    Add word_count task

--- word-count/word_count.h
+++ word-count/word_count.h
@@ -1,8 +1,47 @@
 #pragma once
 
+#include <algorithm>
+#include <cctype>
 #include <string>
-#include <stdexcept>
+#include <unordered_set>
+#include <vector>
+
+namespace {
+std::vector<std::string> Split(const std::string& string) {
+    std::vector<std::string> result;
+
+    if (!string.empty()) {
+        std::string current_string = string;
+        while (true) {
+            auto iterator = std::find_if(current_string.begin(), current_string.end(),
+                                         [](char const& c) { return !std::isalpha(c); });
+
+            result.push_back(current_string.substr(0, iterator - current_string.begin()));
+            if (iterator == current_string.end()) {
+                break;
+            }
+
+            ++iterator;
+            current_string = current_string.substr(iterator - current_string.begin());
+        }
+    }
+
+    return result;
+}
+}  // namespace
 
 int DifferentWordsCount(const std::string& string) {
-    throw std::runtime_error{"Not implemented"};
+    std::string str = string;
+    std::unordered_set<std::string> words;
+
+    std::transform(str.begin(), str.end(), str.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
+    for (auto word : Split(str)) {
+        if (!word.empty()) {
+            words.insert(word);
+        }
+    }
+
+    return words.size();
 }
