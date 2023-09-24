11ecad90fd2923db313c04acc450f58397133a5d
agaevgs_y <agaevgs@yandex-team.ru>
2023-09-24 16:56:18 +0000

    Add range task

--- range/range.h
+++ range/range.h
@@ -1,8 +1,22 @@
 #pragma once
 
 #include <vector>
-#include <stdexcept>
+
+bool IsInRange(int from, int to, int value) {
+    return ((from <= value && value < to) || (from >= value && value > to));
+}
 
 std::vector<int> Range(int from, int to, int step = 1) {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<int> result;
+
+    if ((from > to && step < 0) || (from < to && step > 0)) {
+        for (int value = from;; value += step) {
+            if (!IsInRange(from, to, value)) {
+                break;
+            }
+            result.push_back(value);
+        }
+    }
+
+    return result;
 }
