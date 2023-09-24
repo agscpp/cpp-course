75fabf8cb2c395754119c85a24b6a4298ed7409c
agaevgs_y <agaevgs@yandex-team.ru>
2023-09-24 18:40:19 +0000

    Add split task

--- split/split.h
+++ split/split.h
@@ -1,9 +1,24 @@
 #pragma once
 
 #include <string>
-#include <stdexcept>
 #include <vector>
 
 std::vector<std::string> Split(const std::string& string, const std::string& delimiter = " ") {
-    throw std::runtime_error{"Not implemented"};
+    std::vector<std::string> result;
+
+    if (!string.empty()) {
+        std::string current_string = string;
+        while (true) {
+            std::size_t position = current_string.find(delimiter);
+            result.push_back(current_string.substr(0, position));
+
+            if (position == std::string::npos) {
+                break;
+            }
+
+            current_string = current_string.substr(position + delimiter.size());
+        }
+    }
+
+    return result;
 }
