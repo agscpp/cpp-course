eb98fbef7c812990d6f2809d095a7ecdd79a8b46
agaevgs_y <agaevgs@yandex-team.ru>
2023-09-26 19:26:10 +0000

    Add long_sum task

--- long-sum/long_sum.h
+++ long-sum/long_sum.h
@@ -1,8 +1,38 @@
 #pragma once
 
 #include <string>
-#include <stdexcept>
+
+namespace {
+int GetDigit(const std::string& str, std::size_t index) {
+    return (index < str.size()) ? str[index] - '0' : 0;
+}
+
+std::string ReverseString(const std::string& str) {
+    std::string result;
+
+    for (auto i = str.rbegin(); i != str.rend(); ++i) {
+        result += *i;
+    }
+
+    return result;
+}
+}  // namespace
 
 std::string LongSum(const std::string& a, const std::string& b) {
-    throw std::runtime_error{"Not implemented"};
+    std::string res;
+    std::string reverse_a = ReverseString(a);
+    std::string reverse_b = ReverseString(b);
+
+    int buffer = 0;
+    std::size_t index = 0;
+
+    while (index < reverse_a.size() || index < reverse_b.size() || buffer > 0) {
+        int value = GetDigit(reverse_a, index) + GetDigit(reverse_b, index) + buffer;
+        buffer = value / 10;
+        res += (value % 10) + '0';
+        ++index;
+    }
+
+    res = ReverseString(res);
+    return res;
 }
--- long-sum/test.cpp
+++ long-sum/test.cpp
@@ -2,6 +2,21 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Your test") {
+TEST_CASE("Simple") {
     CHECK(LongSum("2", "2") == "4");
+    CHECK(LongSum("1", "9") == "10");
+    CHECK(LongSum("9", "9") == "18");
+}
+
+TEST_CASE("Empty") {
+    CHECK(LongSum("", "").empty());
+    CHECK(LongSum("132", "") == "132");
+    CHECK(LongSum("", "111") == "111");
+}
+
+TEST_CASE("Big") {
+    CHECK(LongSum("123456789", "123456789") == "246913578");
+    CHECK(LongSum("999999999", "999999999") == "1999999998");
+    CHECK(LongSum("9000000000", "1000000000000000000000000001000000000") ==
+          "1000000000000000000000000010000000000");
 }
