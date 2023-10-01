6b489b9ee9afa594d8602dd29bc59f88ae066284
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-01 20:16:25 +0000

    Add filter-even task

--- filter-even/filter_even.h
+++ filter-even/filter_even.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <cstddef>
 #include <vector>
-#include <stdexcept>
 
 void FilterEven(std::vector<int>* data) {
-    throw std::runtime_error{"Not implemented"};
+    std::size_t odd_number_index = 0;
+    std::size_t data_size = data->size();
+    for (std::size_t index = 0; index < data_size; ++index) {
+        if (data->at(index) % 2 == 0) {
+            std::swap(data->at(index), data->at(odd_number_index));
+            ++odd_number_index;
+        }
+    }
+    while (odd_number_index++ < data_size) {
+        data->pop_back();
+    }
 }
