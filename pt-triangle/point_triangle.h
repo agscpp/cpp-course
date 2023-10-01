7c93ea651781d0c120819eb2f8c6db7968fd58bd
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-01 18:00:00 +0000

    Add pt-triangle task

--- pt-triangle/point_triangle.h
+++ pt-triangle/point_triangle.h
@@ -1,15 +1,37 @@
 #pragma once
 
-#include <stdexcept>
+#include <cmath>
+#include <cstdint>
 
 struct Point {
     int x, y;
+
+    bool operator==(const Point& rhs) const {
+        return this->x == rhs.x && this->y == rhs.y;
+    }
 };
 
 struct Triangle {
     Point a, b, c;
 };
 
-bool IsPointInTriangle(const Triangle& t, const Point& pt) {
-    throw std::runtime_error{"Not implemented"};
+double TriangleArea(const Point& a, const Point& b, const Point& c) {
+    int64_t a_x = static_cast<int64_t>(a.x);
+    int64_t a_y = static_cast<int64_t>(a.y);
+    int64_t b_x = static_cast<int64_t>(b.x);
+    int64_t b_y = static_cast<int64_t>(b.y);
+    int64_t c_x = static_cast<int64_t>(c.x);
+    int64_t c_y = static_cast<int64_t>(c.y);
+    return std::abs((b_x - a_x) * (c_y - a_y) - (c_x - a_x) * (b_y - a_y)) / 2.0;
+}
+
+double IsPointInTriangle(const Triangle& triangle, const Point& point) {
+    if (triangle.a == triangle.b && triangle.b == triangle.c) {
+        return false;
+    }
+    double abp = TriangleArea(triangle.a, triangle.b, point);
+    double bcp = TriangleArea(triangle.b, triangle.c, point);
+    double cap = TriangleArea(triangle.c, triangle.a, point);
+    double abc = TriangleArea(triangle.a, triangle.b, triangle.c);
+    return abc == abp + bcp + cap;
 }
--- pt-triangle/test.cpp
+++ pt-triangle/test.cpp
@@ -2,5 +2,16 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Your test") {
+TEST_CASE("Simple") {
+    Triangle triangle{Point{2, -17}, Point{5, -5}, Point{-5, -10}};
+
+    CHECK(IsPointInTriangle(triangle, {-3, -10}));
+    CHECK_FALSE(IsPointInTriangle(triangle, {-3, 10}));
+    CHECK_FALSE(IsPointInTriangle(triangle, {2, 5}));
+    CHECK_FALSE(IsPointInTriangle(triangle, {-2, -5}));
+}
+
+TEST_CASE("Zero") {
+    Triangle triangle{Point{1, -1}, Point{1, -1}, Point{1, -1}};
+    CHECK_FALSE(IsPointInTriangle(triangle, {-1, -1}));
 }
