73645e277e98887c129e2a84fe5d826b4edaf2cd
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-06 18:09:15 +0000

    Add rotate task

--- rotate/rotate.h
+++ rotate/rotate.h
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <stdexcept>
 #include <vector>
-#include <cstddef>
 
 namespace {
 void Swap(int* a, int* b) {
@@ -10,7 +8,19 @@ void Swap(int* a, int* b) {
     *b ^= *a;
     *a ^= *b;
 }
+
+void Reverse(std::size_t left, std::size_t right, std::vector<int>* data) {
+    while (left < right) {
+        Swap(&(data->at(left++)), &(data->at(right--)));
+    }
+}
 }  // namespace
 
-void Rotate(std::vector<int>* data, size_t shift) {
+void Rotate(std::vector<int>* data, std::size_t shift) {
+    if (shift == 0 || data->empty() || data->size() == shift) {
+        return;
+    }
+    Reverse(0, shift - 1, data);
+    Reverse(shift, data->size() - 1, data);
+    Reverse(0, data->size() - 1, data);
 }
