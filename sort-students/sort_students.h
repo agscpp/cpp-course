f85d7f016a10311d409d90cd1517e9b2ad991edb
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-08 08:37:39 +0000

    Add sort-students task

--- sort-students/sort_students.h
+++ sort-students/sort_students.h
@@ -1,16 +1,63 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 #include <string>
-#include <stdexcept>
+#include <ranges>
 
 struct Student {
     std::string name, surname;
     int day, month, year;
+
+    bool operator==(const Student& rhs) const {
+        return this->name == rhs.name && this->surname == rhs.surname && this->year == rhs.year &&
+               this->month == rhs.month && this->day == rhs.day;
+    }
 };
 
 enum class SortType { kByName, kByDate };
 
+namespace {
+bool CompareByName(const Student& left, const Student& right) {
+    if (left.surname == right.surname) {
+        if (left.name == right.name) {
+            if (left.year == right.year) {
+                if (left.month == right.month) {
+                    return left.day < right.day;
+                }
+                return left.month < right.month;
+            }
+            return left.year < right.year;
+        }
+        return left.name < right.name;
+    }
+    return left.surname < right.surname;
+}
+
+bool CompareByDate(const Student& left, Student& right) {
+    if (left.year == right.year) {
+        if (left.month == right.month) {
+            if (left.day == right.day) {
+                if (left.surname == right.surname) {
+                    return left.name < right.name;
+                }
+                return left.surname < right.surname;
+            }
+            return left.day < right.day;
+        }
+        return left.month < right.month;
+    }
+    return left.year < right.year;
+}
+}  // namespace
+
 void SortStudents(std::vector<Student>* students, SortType sort_type) {
-    throw std::runtime_error{"Not implemented"};
+    switch (sort_type) {
+        case SortType::kByName:
+            std::ranges::sort((*students).begin(), (*students).end(), CompareByName);
+            break;
+        case SortType::kByDate:
+            std::ranges::sort((*students).begin(), (*students).end(), CompareByDate);
+            break;
+    }
 }
--- sort-students/test.cpp
+++ sort-students/test.cpp
@@ -1,6 +1,47 @@
 #include <sort_students.h>
 
 #include <catch2/catch_test_macros.hpp>
+#include <vector>
 
-TEST_CASE("Your test") {
+TEST_CASE("Signature") {
+    INFO("Do not change SortStydents signature");
+    STATIC_CHECK(
+        std::is_same_v<decltype(&SortStudents), void (*)(std::vector<Student>*, SortType)>);
+}
+
+TEST_CASE("Simple") {
+    std::vector<Student> students = {
+        Student{"Ivan", "Ivanov", 2000, 12, 31},
+        Student{"Ray", "William", 2000, 1, 12},
+        Student{"Peter", "Harper", 2010, 5, 30},
+        Student{"Ethan", "Mosley", 2000, 12, 31},
+    };
+
+    {
+        SortStudents(&students, SortType::kByDate);
+        CHECK(students == std::vector<Student>({
+                              Student{"Ray", "William", 2000, 1, 12},
+                              Student{"Ivan", "Ivanov", 2000, 12, 31},
+                              Student{"Ethan", "Mosley", 2000, 12, 31},
+                              Student{"Peter", "Harper", 2010, 5, 30},
+                          }));
+    }
+    {
+        SortStudents(&students, SortType::kByName);
+        CHECK(students == std::vector<Student>({
+                              Student{"Peter", "Harper", 2010, 5, 30},
+                              Student{"Ivan", "Ivanov", 2000, 12, 31},
+                              Student{"Ethan", "Mosley", 2000, 12, 31},
+                              Student{"Ray", "William", 2000, 1, 12},
+                          }));
+    }
+}
+
+TEST_CASE("Empty") {
+    {
+        std::vector<Student> students;
+        SortStudents(&students, SortType::kByDate);
+        SortStudents(&students, SortType::kByName);
+        CHECK(students.empty());
+    }
 }
