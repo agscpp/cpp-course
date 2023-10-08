b231ddff997285bab3b4a1166a5aa045e4e12920
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-08 18:39:40 +0000

    Add ring-buffer task

--- ring-buffer/ring_buffer.h
+++ ring-buffer/ring_buffer.h
@@ -1,25 +1,49 @@
 #pragma once
 
 #include <cstddef>
+#include <vector>
 
 class RingBuffer {
 public:
-    explicit RingBuffer(size_t capacity) {
+    explicit RingBuffer(std::size_t capacity)
+        : capacity_(capacity), write_index_(0), read_index_(0), size_(0) {
+        elements_.resize(capacity, 0);
     }
 
     size_t Size() const {
-        return 0;
+        return size_;
     }
 
     bool Empty() const {
-        return false;
+        return size_ == 0;
     }
 
     bool TryPush(int element) {
+        if (Size() == capacity_) {
+            return false;
+        }
+
+        ++size_;
+        write_index_ = (write_index_ < capacity_) ? write_index_ : 0;
+        elements_[write_index_++] = element;
         return true;
     }
 
     bool TryPop(int* element) {
+        if (Empty()) {
+            return false;
+        }
+
+        --size_;
+        read_index_ = (read_index_ < capacity_) ? read_index_ : 0;
+        *element = elements_[read_index_++];
         return true;
     }
+
+private:
+    std::size_t capacity_;
+    std::size_t write_index_;
+    std::size_t read_index_;
+    std::size_t size_;
+    std::vector<int> elements_;
 };
