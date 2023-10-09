06b962b80ac5303c53ab4098b0aeaf5bca7247e9
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-09 08:40:57 +0000

    Add static_map task

--- static-map/static_map.h
+++ static-map/static_map.h
@@ -1,15 +1,44 @@
 #pragma once
 
+#include <algorithm>
 #include <vector>
 #include <string>
-#include <utility>
 
 class StaticMap {
 public:
     explicit StaticMap(const std::vector<std::pair<std::string, std::string>>& items) {
+        items_ = items;
+        std::sort(items_.begin(), items_.end(), CompareByKey);
     }
 
     bool Find(const std::string& key, std::string* value) const {
+        if (!items_.empty()) {
+            int64_t left = 0;
+            int64_t right = items_.size() - 1;
+
+            while (left <= right) {
+                int64_t middle = (right + left) / 2;
+                const std::pair<std::string, std::string>& item = items_[middle];
+
+                if (item.first < key) {
+                    left = middle + 1;
+                } else if (item.first > key) {
+                    right = middle - 1;
+                } else {
+                    *value = item.second;
+                    return true;
+                }
+            }
+        }
+
         return false;
+    }
+
+private:
+    std::vector<std::pair<std::string, std::string>> items_;
+
+    static bool CompareByKey(const std::pair<std::string, std::string>& left,
+                             const std::pair<std::string, std::string>& right) {
+        return left.first < right.first;
     }
 };
