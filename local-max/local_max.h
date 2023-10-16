7abbc7d799c0d4882b2aa1e5e8522bbd44417e66
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-16 10:23:42 +0000

    Add local_max task

--- local-max/local_max.h
+++ local-max/local_max.h
@@ -1,8 +1,31 @@
 #pragma once
 
-#include <stdexcept>
-
 template <class Iterator>
 Iterator LocalMax(Iterator first, Iterator last) {
-    throw std::runtime_error{"Not implemented"};
+    if (first == last) {
+        return last;
+    }
+
+    auto answer = first;
+    auto first_min = first;
+
+    while (++first != last) {
+        if (answer == last) {
+            if (*first_min < *first) {
+                answer = first;
+            } else {
+                first_min = first;
+            }
+        } else if (*first < *answer) {
+            return answer;
+        } else if (*answer < *first) {
+            first_min = answer;
+            answer = first;
+        } else {
+            first_min = first;
+            answer = last;
+        }
+    }
+
+    return answer;
 }
--- local-max/test.cpp
+++ local-max/test.cpp
@@ -2,5 +2,43 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Your test") {
+TEST_CASE("Simple") {
+    {
+        auto data = {0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0};
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(*answer == 5);
+    }
+    {
+        auto data = {1};
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(*answer == 1);
+    }
+    {
+        auto data = {0, 1};
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(*answer == 1);
+    }
+    {
+        auto data = {1, 0};
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(*answer == 1);
+    }
+    {
+        auto data = {1, 2, 1, 5, 3};
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(*answer == 2);
+    }
+}
+
+TEST_CASE("Emtpy") {
+    {
+        std::vector<int> data;
+        auto answer = LocalMax(data.begin(), data.end());
+        REQUIRE(answer == data.end());
+    }
+    {
+        auto data = {1, 2, 1};
+        auto answer = LocalMax(data.begin(), data.begin());
+        REQUIRE(answer == data.begin());
+    }
 }
