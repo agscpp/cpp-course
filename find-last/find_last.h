251c2e6491696ad1956624289ce81b3ea73e96eb
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-16 08:43:50 +0000

    Add find_last task

--- find-last/find_last.h
+++ find-last/find_last.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#include <stdexcept>
-
 template <class Iterator, class T>
 Iterator FindLast(Iterator first, Iterator last, const T& value) {
-    throw std::runtime_error{"Not implemented"};
+    auto answer = last;
+    while (first != answer) {
+        if (value == *(--answer)) {
+            return answer;
+        }
+    }
+    return last;
 }
