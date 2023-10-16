965b7a66f9478df6337ce17a9c98309ca033fd9d
agaevgs_y <agaevgs@yandex-team.ru>
2023-10-16 12:57:37 +0000

    Add fold task

--- fold/fold.h
+++ fold/fold.h
@@ -1,13 +1,48 @@
 #pragma once
 
-struct Sum {};
+#include <cstddef>
+#include <vector>
 
-struct Prod {};
+struct Sum {
+    template <class T>
+    void operator()(T* a, const T& b) {
+        *a += b;
+    }
+};
 
-struct Concat {};
+struct Prod {
+    template <class T>
+    void operator()(T* a, const T& b) {
+        *a *= b;
+    }
+};
 
-class Length {};
+struct Concat {
+    template <class T>
+    void operator()(std::vector<T>* a, const std::vector<T>& b) {
+        a->insert(a->end(), b.begin(), b.end());
+    }
+};
+
+class Length {
+private:
+    size_t* value_;
+
+public:
+    Length() = delete;
+    Length(size_t* value) : value_(value) {
+    }
+
+    template <class T>
+    void operator()(T*, const T&) {
+        ++(*value_);
+    }
+};
 
 template <class Iterator, class T, class BinaryOp>
 T Fold(Iterator first, Iterator last, T init, BinaryOp func) {
+    while (first != last) {
+        func(&init, *first++);
+    }
+    return init;
 }
