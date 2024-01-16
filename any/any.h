537b2914de5ff6819317f7db913401134684d971
agaevgs_y <agaevgs@yandex-team.ru>
2024-01-16 23:59:06 +0000

    Add task any

--- any/any.h
+++ any/any.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <type_traits>
+#include <new>
 #include <concepts>
+#include <typeinfo>
+#include <utility>
 
 template <class T>
 concept NotAny = !std::same_as<std::remove_cvref_t<T>, class Any>;
@@ -11,33 +13,88 @@ public:
     Any() {
     }
 
-    // T&& - universal (forwarding) reference
-    // use std::forward inside this constructor
     template <NotAny T>
-    Any(T&& value) {
+    Any(T&& value) : holder_(new ValueHolder(std::forward<T>(value))) {
     }
 
     Any(const Any& other) {
+        if (other.holder_) {
+            holder_ = other.holder_->Clone();
+        }
     }
-    Any(Any&& other) {
+
+    Any(Any&& other) : holder_(other.holder_) {
+        other.holder_ = nullptr;
     }
+
     Any& operator=(const Any& other) {
+        if (this != &other) {
+            auto new_any = other;
+            Swap(new_any);
+        }
+        return *this;
     }
+
     Any& operator=(Any&& other) {
+        if (this != &other) {
+            Swap(other);
+        }
+        return *this;
     }
+
     ~Any() {
+        Clear();
     }
 
     bool Empty() const {
-        return true;
+        return holder_ == nullptr;
     }
 
     void Clear() {
+        delete holder_;
+        holder_ = nullptr;
     }
     void Swap(Any& other) {
+        std::swap(holder_, other.holder_);
     }
 
     template <class T>
     const T& GetValue() const {
+        auto* ptr = dynamic_cast<ValueHolder<T>*>(holder_);
+        if (ptr) {
+            return *(*ptr);
+        }
+        throw std::bad_cast();
     }
+
+private:
+    class ValueHolderBase {
+    public:
+        virtual ~ValueHolderBase() = default;
+        virtual ValueHolderBase* Clone() = 0;
+    };
+
+    template <typename T>
+    class ValueHolder : public ValueHolderBase {
+    public:
+        ValueHolder(T value) : value_(std::move(value)) {
+        }
+
+        T& operator*() {
+            return value_;
+        }
+
+        const T& operator*() const {
+            return value_;
+        }
+
+        ValueHolder<T>* Clone() override {
+            return new ValueHolder<T>(value_);
+        }
+
+    private:
+        T value_;
+    };
+
+    ValueHolderBase* holder_ = nullptr;
 };
